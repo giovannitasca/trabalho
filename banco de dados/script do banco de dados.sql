dde00cbd08161512ea32d14abed4d65839e5e7eb
aluno <aluno@labivj.info>
2017-10-06 18:42:54 -0300

    Enviando a pasta com o script do banco de dados;

---
+++ banco de dados/script do banco de dados.sql
@@ -0,0 +1,10 @@
+create database db_trabalho;
+use db_trabalho;
+
+create table tbusuarios(
+iduser int primary key,
+nomeuser varchar(45),
+sobrenomeuser varchar(45),
+senhauser varchar(45),
+emailuser varchar(45)
+);
