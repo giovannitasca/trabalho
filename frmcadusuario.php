70282bb2e9c4ba88668f7beeb5ef588153b3af12
aluno <aluno@labivj.info>
2017-10-06 18:40:28 -0300

    Enviando o formulario de login(index.php); Enviando o formulário de cadastro de usuários (frmcadusuario.php);

---
+++ frmcadusuario.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<HTML lang = "PT-BR">
+<HEAD>
+ <meta charset ="UTF-8">
+ <TITLE>Novo Documento</TITLE>
+</HEAD>
+<BODY>
+
+  <FORM ACTION = "Cadastrousuario.php" METHOD = "POST">
+        <h1 align = "center"><font color = "#C41502"><u>Cadastro de Usuários</u></font></h1> <BR> <BR>
+  <BR>
+        <p>Usuário: <BR> <input type="text" size="30" name = "txtusuario" maxlength = 30>
+
+        <p>Sobrenome: <BR> <input type="text" size="30" name = "txtsobrenome" maxlength = 30>
+        
+        <p>Senha: <BR>  <input type="password" size="30" name = "txtsenha" maxlength = 30><BR><BR>
+        
+        <p>Confirma Senha: <BR> <input type="password" size="30" name = "txtconfirma" maxlength = 30><BR><BR>
+        
+        <p>Email:<BR> <input type="text" size="45" name = "txtemail" maxlength = 44><BR><BR><BR>
+  
+        <input type="submit" value="Cadastrar"> <BR><BR>
+        
+        <A HREF = 'index.php'>Logar no Site</a>
+        
+  </FORM>
+  
+</BODY>
+</HTML>
---
+++ index.php
@@ -0,0 +1,21 @@
+<HTML>
+<HEAD>
+ <TITLE>Documento PHP</TITLE>
+</HEAD>
+
+<BODY bgcolor = "silver">
+
+  <FORM ACTION = "conectarlogin.php" METHOD = "POST">
+
+      <BR><BR><BR><BR><BR><BR>
+      <p align = center ><font size = 4>Login: &nbsp;</font><input type="text" size="30" name = "txtusuario" maxlength = 30>
+      <p align = center><font size = 4>Senha: </font><input type="password" size="30" name = "txtsenha" maxlength = 30> <BR><BR>
+      
+      <input type = "submit" value = "Logar"></p><BR>
+      
+      <A HREF = 'frmcadusuario.php'>Cadastrar</a>
+
+  </FORM>
+
+</BODY>
+</HTML>
